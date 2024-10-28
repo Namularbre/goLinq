bdbf7c3ec7791046f777ab0dc68913498c889b1f
namu <namulabre@gmail.com>
2024-10-28 23:43:16 +0100

    Changed filter parameter name to predicate to match C# linq

--- where.go
+++ where.go
@@ -1,7 +1,7 @@
 package linq
 
 // Where is used to make a projection of a new slice where all elements match the filter
-func Where[T any](input []T, filter func(T) bool) []T {
+func Where[T any](input []T, predicate func(T) bool) []T {
 	var output []T
 
 	for _, inputElem := range input {
@@ -10,7 +10,7 @@ func Where[T any](input []T, filter func(T) bool) []T {
 		go func() {
 			defer close(ok)
 
-			ok <- filter(inputElem)
+			ok <- predicate(inputElem)
 		}()
 
 		if <-ok {

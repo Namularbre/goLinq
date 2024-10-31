852f8fe4bbb17ada4b7bcd5e5f6da97c6932b751
namu <namulabre@gmail.com>
2024-11-01 00:40:40 +0100

    Simplified Skip func

--- skip.go
+++ skip.go
@@ -2,19 +2,8 @@ package linq
 
 // Skip skips x element in a slice
 func Skip[T any](input []T, x uint) []T {
-	var output []T
-
-	skip := true
-
-	for iter, elem := range input {
-		if uint(iter) == x {
-			skip = false
-		}
-
-		if !skip {
-			output = append(output, elem)
-		}
+	if x >= uint(len(input)) {
+		return []T{}
 	}
-
-	return output
+	return input[x:]
 }
